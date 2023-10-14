89d89628d50228c047718741f2a09c8c0d3329c0
Bereket Semagn <semagnbereket@gmail.com>
2023-10-14 14:56:49 -0400

    NEXT_PUBLIC

--- lib/constants.ts
+++ lib/constants.ts
@@ -1,8 +1,8 @@
 export const API_URL = process.env.API_URL;
-export const FRONTEND_URL = process.env.FRONTEND_URL
+export const FRONTEND_URL = process.env.NEXT_PUBLIC_FRONTEND_URL;
 export const GITHUB_URL = "https://github.com/hitsapp/hits";
 
 export const creators = [
-    { name: "bereket", github: "https://github.com/heybereket" },
-    { name: "cody", github: "https://github.com/looskie" },
-  ];
+  { name: "bereket", github: "https://github.com/heybereket" },
+  { name: "cody", github: "https://github.com/looskie" },
+];
